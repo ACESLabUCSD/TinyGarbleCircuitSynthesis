15220279e1d63e19360c3d69f2a5a12bc27e5286
siamumar <s2hussai@ieng6-245.ucsd.edu>
2019-04-09 11:57:03 -0700

    add FADDER to count

--- SynthesisLibrary/script/count.sh
+++ SynthesisLibrary/script/count.sh
@@ -14,12 +14,13 @@ grep -w -c 'MUX' $1
 grep -w -c 'FA' $1
 grep -w -c 'HA' $1
 grep -w -c 'HADDER' $1
+grep -w -c 'FADDER' $1
 grep -w -c 'XOR' $1
 grep -w -c 'XNOR' $1
 grep -w -c 'IV' $1
 grep -w -c 'DFF' $1
 
 echo -n "total non-XOR: "
-grep -w -c 'AND\|ANDN\|NAND\|NANDN\|OR\|ORN\|NOR\|NORN\|MUX\|FA\|HA\|HADDER' $1
+grep -w -c 'AND\|ANDN\|NAND\|NANDN\|OR\|ORN\|NOR\|NORN\|MUX\|FA\|HA\|HADDER\|FADDER' $1
 echo -n "total XOR: "
 grep -w -c 'XOR\|XNOR\|IV\|DFF' $1
