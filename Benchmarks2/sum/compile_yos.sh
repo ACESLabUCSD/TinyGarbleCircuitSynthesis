b7f47ceff3c59b1b2480ef4f73fd3b79a5aedeab
Hussain, Siam <siam.hussain@intel.com>
2019-07-26 17:01:35 -0700

    add yosys syn files for aes comb, seq does not work with init

--- Benchmarks2/sum/compile_yos.sh
+++ Benchmarks2/sum/compile_yos.sh
@@ -1,3 +1,7 @@
 mkdir -p syn
+
 yosys -s sum_nbit_1cc.yos 
 yosys -s sum_nbit_ncc.yos
+
+../../Verilog2EMP/bin/V2EMP_Main -i syn/sum_nbit_1cc_syn_yos.v -o syn/sum_nbit_1cc_syn_yos.emp --log2std
+../../Verilog2EMP/bin/V2EMP_Main -i syn/sum_nbit_ncc_syn_yos.v -o syn/sum_nbit_ncc_syn_yos.emp --log2std
