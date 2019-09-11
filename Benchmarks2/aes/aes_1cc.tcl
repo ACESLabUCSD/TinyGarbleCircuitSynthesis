c683ac3315cafc9a1578e184064849c38f84a733
siam.hussain <siamhuss@intel.com>
2019-09-11 09:47:37 -0700

    updated yosys files for aes

--- Benchmarks2/aes/aes_1cc.tcl
+++ Benchmarks2/aes/aes_1cc.tcl
@@ -1,12 +1,14 @@
+yosys -import
+
 read_verilog ../../SynthesisLibrary/syn_lib/*.v
 
 read_verilog -sv aes_1cc.sv  AddRoundKey.sv  KeyExpansionSeq.sv  KeyExpansion.sv  MixColumns.sv  ShiftRows.sv  SubBytes.sv
 hierarchy -check -top aes_1cc
-proc; opt; flatten; opt; 
+procs; opt; flatten; opt; 
 techmap ; opt;
 dfflibmap -liberty ../../SynthesisLibrary/lib_EMP/asic_cell_yosys.lib
 abc -liberty ../../SynthesisLibrary/lib_EMP/asic_cell_yosys.lib -script ../../SynthesisLibrary/lib_EMP/script.abc; 
 opt; clean; opt;
 opt_clean -purge
-stat -liberty ../../SynthesisLibrary/lib/asic_cell_yosys.lib
-write_verilog -noattr -noexpr syn/aes_1cc_syn_yos.v
+stat -liberty ../../SynthesisLibrary/lib_EMP/asic_cell_yosys_area.lib
+write_verilog -noattr -noexpr -nohex syn/aes_1cc_syn.v
