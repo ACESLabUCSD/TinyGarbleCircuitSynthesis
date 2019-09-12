ea36f20f9d6ded3ad8b752e045cad90f2841b43c
siam.hussain <siamhuss@intel.com>
2019-09-12 16:48:03 -0700

    update the number of bits in relu for fc

--- Benchmarks2/relu/relu.tcl
+++ Benchmarks2/relu/relu.tcl
@@ -1,6 +1,6 @@
 yosys -import
 
-foreach N [list 9 17 19 21 33 35 37 65 67 69] {
+foreach N [list 9 17 19 21 23 33 35 37 39 65 67 69 71] {
 	read_verilog -overwrite -defer ../../SynthesisLibrary/syn_lib/*.v 
 	read_verilog -overwrite -defer -sv  relu_nbit_1cc.sv
 	hierarchy -check -top relu_nbit_1cc -chparam N $N 
