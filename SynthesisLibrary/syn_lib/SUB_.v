de9c7f437756c7933d07dd1b8337c938e5dc4365
siamumar <siamumar@ucsd.edu>
2020-05-05 21:12:08 -0700

    add function for conditional add/sub

--- SynthesisLibrary/syn_lib/SUB_.v
+++ SynthesisLibrary/syn_lib/SUB_.v
@@ -59,3 +59,32 @@ module uSUB_ #(parameter N = 8, M = N)( // N >= M
 		.CO(CO)
 );
 endmodule
+
+module uADD_SUB_ #(parameter N = 8, M = N)( // N >= M
+	input [N-1:0] A,
+	input [M-1:0] B,
+	input s,
+	output [N:0] O
+    );
+	 
+	 wire [N-1:0] BB;
+	 wire CO;
+	 
+	 generate
+		if (N > M)
+			assign BB = {{(N-M){1'b0}}, B};
+		else
+			assign BB = B;
+	 endgenerate
+	
+  	assign O[N] = ~CO;
+	 
+	ADD #(.N(N)) SUB(
+		.A(A),
+		.B(BB^{N{s}}),
+        .CI(s),
+		.S(O[N-1:0]), 
+		.CO(CO)
+);
+endmodule
+
